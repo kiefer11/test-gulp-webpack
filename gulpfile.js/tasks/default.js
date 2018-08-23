046a636cf9234b3254ce720198e9066da96d497d
松下 敦 <atsushi.matsushita@d2cdot.co.jp>
2018-08-23 20:21:13 +0900

    gulpのtaskを分割できるように変更

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -3,12 +3,15 @@ const gulp = require("gulp");
 const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
 
+// gulpの設定ファイル
+const config = require("../config");
+
 // webpackの設定ファイルの読み込み
-const webpackConfig = require("./build/webpack.config");
+const webpackConfig = require("../../build/webpack.config");
 
 // タスクの定義。 ()=> の部分はfunction() でも可
 gulp.task("default", () => {
   // ☆ webpackStreamの第2引数にwebpackを渡す☆
   return webpackStream(webpackConfig, webpack)
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest(config.root.dest));
 });
